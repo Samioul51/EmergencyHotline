b80d7af93af54c434059a851ef4d841deeff2274
Samioul51 <akmsamioulislam51@gmail.com>
2025-08-28 00:42:54 +0600

    Clicking on the  heart icon of any card will increase the count in the Navbar task completed

---
+++ script.js
@@ -0,0 +1,64 @@
+// Task-4
+
+document.getElementById("fav1").addEventListener("click",function(e){
+    const curHeart=document.getElementById("curHeart");
+    let heartAmount=parseInt(curHeart.innerText);
+    heartAmount++;
+    curHeart.innerText=heartAmount.toString();
+});
+
+document.getElementById("fav2").addEventListener("click",function(e){
+    const curHeart=document.getElementById("curHeart");
+    let heartAmount=parseInt(curHeart.innerText);
+    heartAmount++;
+    curHeart.innerText=heartAmount.toString();
+});
+
+document.getElementById("fav3").addEventListener("click",function(e){
+    const curHeart=document.getElementById("curHeart");
+    let heartAmount=parseInt(curHeart.innerText);
+    heartAmount++;
+    curHeart.innerText=heartAmount.toString();
+});
+
+document.getElementById("fav4").addEventListener("click",function(e){
+    const curHeart=document.getElementById("curHeart");
+    let heartAmount=parseInt(curHeart.innerText);
+    heartAmount++;
+    curHeart.innerText=heartAmount.toString();
+});
+
+document.getElementById("fav5").addEventListener("click",function(e){
+    const curHeart=document.getElementById("curHeart");
+    let heartAmount=parseInt(curHeart.innerText);
+    heartAmount++;
+    curHeart.innerText=heartAmount.toString();
+});
+
+document.getElementById("fav6").addEventListener("click",function(e){
+    const curHeart=document.getElementById("curHeart");
+    let heartAmount=parseInt(curHeart.innerText);
+    heartAmount++;
+    curHeart.innerText=heartAmount.toString();
+});
+
+document.getElementById("fav7").addEventListener("click",function(e){
+    const curHeart=document.getElementById("curHeart");
+    let heartAmount=parseInt(curHeart.innerText);
+    heartAmount++;
+    curHeart.innerText=heartAmount.toString();
+});
+
+document.getElementById("fav8").addEventListener("click",function(e){
+    const curHeart=document.getElementById("curHeart");
+    let heartAmount=parseInt(curHeart.innerText);
+    heartAmount++;
+    curHeart.innerText=heartAmount.toString();
+});
+
+document.getElementById("fav9").addEventListener("click",function(e){
+    const curHeart=document.getElementById("curHeart");
+    let heartAmount=parseInt(curHeart.innerText);
+    heartAmount++;
+    curHeart.innerText=heartAmount.toString();
+});
