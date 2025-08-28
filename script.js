e723e089585dd1ce2169688e131e1b7864fd45d0
Samioul51 <akmsamioulislam51@gmail.com>
2025-08-28 19:26:10 +0600

    Task-6 Completed

--- script.js
+++ script.js
@@ -11,10 +11,16 @@ for(const fav of favs){
     });
 }
 
-//Task-5
+//Task-5 and Task-6
 
 const calls=document.querySelectorAll(".call");
 const coins=document.getElementById("coin");
+const callContainer=document.querySelector(".call-container");
+const clearHistory=document.querySelector(".clearHistory");
+
+clearHistory.addEventListener("click",function(){
+    callContainer.innerHTML="";
+});
 
 for(const call of calls){
     call.addEventListener("click",function(e){
@@ -24,10 +30,50 @@ for(const call of calls){
             const superParent=call.parentElement.parentElement;
             const title=superParent.querySelector(".title");
             const number=superParent.querySelector(".number");
-            console.log(number);
             alert("📞 Calling "+title.innerText+" "+number.innerText+"...");
             curCoins-=20;
             coins.innerText=curCoins.toString();
+
+            //Task-6 started
+            const innerDiv=document.createElement("div");
+            const serviceName=document.createElement("p");
+            const serviceNumber=document.createElement("p");
+            serviceName.innerText=title.innerText;
+            serviceNumber.innerText=number.innerText;
+            innerDiv.classList.add("flex","flex-col","gap-[4px]");
+            serviceName.classList.add("font-semibold","text-[18px]");
+            serviceNumber.classList.add("text-[18px]","text-[#808080]");
+            innerDiv.appendChild(serviceName);
+            innerDiv.appendChild(serviceNumber);
+            const time=new Date();
+            let hours=time.getHours();
+            let mins=time.getMinutes();
+            let seconds=time.getSeconds();
+            let dummy,counter1=0,counter2=0;
+            if(hours>12){
+                hours%=12;
+                dummy=" PM";
+            }
+            else
+                dummy=" AM";
+            if(mins<10)
+                counter1=1;
+            if(seconds<10)
+                counter2=1;
+            const timePara=document.createElement("p");
+            hours=hours.toString();
+            mins=mins.toString();
+            seconds=seconds.toString();
+            if(counter1)
+                mins="0"+mins;
+            if(counter2)
+                seconds="0"+seconds;
+            timePara.innerText=hours+":"+mins+":"+seconds+dummy;
+            const outerDiv=document.createElement("div");
+            outerDiv.appendChild(innerDiv);
+            outerDiv.appendChild(timePara);
+            outerDiv.classList.add("flex","justify-between","items-center","rounded-[8px]","bg-[#fafafa]","mb-[8px]","p-[16px]");
+            callContainer.appendChild(outerDiv);
         }
         else
             alert("You don't have enough coins. You need at least 20 coins to make a call.");
