07f43db27a94ae9b5c4c3841024f96ef13ba758e
Samioul51 <akmsamioulislam51@gmail.com>
2025-08-28 18:23:30 +0600

    Simplified Task-4 and Completed Task-5

--- script.js
+++ script.js
@@ -1,64 +1,35 @@
 // Task-4
 
-document.getElementById("fav1").addEventListener("click",function(e){
-    const curHeart=document.getElementById("curHeart");
-    let heartAmount=parseInt(curHeart.innerText);
-    heartAmount++;
-    curHeart.innerText=heartAmount.toString();
-});
+const favs=document.querySelectorAll(".favourite");
 
-document.getElementById("fav2").addEventListener("click",function(e){
-    const curHeart=document.getElementById("curHeart");
-    let heartAmount=parseInt(curHeart.innerText);
-    heartAmount++;
-    curHeart.innerText=heartAmount.toString();
-});
+for(const fav of favs){
+        fav.addEventListener("click",function(e){
+        const curHeart=document.getElementById("curHeart");
+        let heartAmount=parseInt(curHeart.innerText);
+        heartAmount++;
+        curHeart.innerText=heartAmount.toString();
+    });
+}
 
-document.getElementById("fav3").addEventListener("click",function(e){
-    const curHeart=document.getElementById("curHeart");
-    let heartAmount=parseInt(curHeart.innerText);
-    heartAmount++;
-    curHeart.innerText=heartAmount.toString();
-});
+//Task-5
 
-document.getElementById("fav4").addEventListener("click",function(e){
-    const curHeart=document.getElementById("curHeart");
-    let heartAmount=parseInt(curHeart.innerText);
-    heartAmount++;
-    curHeart.innerText=heartAmount.toString();
-});
+const calls=document.querySelectorAll(".call");
+const coins=document.getElementById("coin");
 
-document.getElementById("fav5").addEventListener("click",function(e){
-    const curHeart=document.getElementById("curHeart");
-    let heartAmount=parseInt(curHeart.innerText);
-    heartAmount++;
-    curHeart.innerText=heartAmount.toString();
-});
-
-document.getElementById("fav6").addEventListener("click",function(e){
-    const curHeart=document.getElementById("curHeart");
-    let heartAmount=parseInt(curHeart.innerText);
-    heartAmount++;
-    curHeart.innerText=heartAmount.toString();
-});
-
-document.getElementById("fav7").addEventListener("click",function(e){
-    const curHeart=document.getElementById("curHeart");
-    let heartAmount=parseInt(curHeart.innerText);
-    heartAmount++;
-    curHeart.innerText=heartAmount.toString();
-});
-
-document.getElementById("fav8").addEventListener("click",function(e){
-    const curHeart=document.getElementById("curHeart");
-    let heartAmount=parseInt(curHeart.innerText);
-    heartAmount++;
-    curHeart.innerText=heartAmount.toString();
-});
-
-document.getElementById("fav9").addEventListener("click",function(e){
-    const curHeart=document.getElementById("curHeart");
-    let heartAmount=parseInt(curHeart.innerText);
-    heartAmount++;
-    curHeart.innerText=heartAmount.toString();
-});
+for(const call of calls){
+    call.addEventListener("click",function(e){
+        e.preventDefault();
+        let curCoins=parseInt(coins.innerText);
+        if(curCoins>=20){
+            const superParent=call.parentElement.parentElement;
+            const title=superParent.querySelector(".title");
+            const number=superParent.querySelector(".number");
+            console.log(number);
+            alert("📞 Calling "+title.innerText+" "+number.innerText+"...");
+            curCoins-=20;
+            coins.innerText=curCoins.toString();
+        }
+        else
+            alert("You don't have enough coins. You need at least 20 coins to make a call.");
+    });
+}
